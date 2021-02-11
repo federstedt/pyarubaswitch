e139cf61fa874309d82e05a3237c3f8baedb7829
Daniel F <daniel.federstedt@nordlo.comwq>
2021-02-11 13:54:35 +0100

    included 1 more function

--- pyarubaswitch/__init__.py
+++ pyarubaswitch/__init__.py
@@ -1,7 +1,9 @@
 from pyarubaswitch.aruba_switch_client import ArubaSwitchClient
 from pyarubaswitch.api_engine import PyAosSwitch
+from pyarubaswitch.common_ops import shorten_numberlist
 
 __all__ = [
     'ArubaSwitchClient',
-    'PyAosSwitch'
+    'PyAosSwitch',
+    'shorten_numberlist'
 ]
