c3e1c56dd37bfab0baf42b937ab0e0ba1105b1de
Daniel F <daniel.federstedt@nordlo.comwq>
2021-02-11 11:20:26 +0100

    removed comments

--- pyarubaswitch/__init__.py
+++ pyarubaswitch/__init__.py
@@ -5,23 +5,3 @@ __all__ = [
     'ArubaSwitchClient',
     'PyAosSwitch'
 ]
-
-'''
-from phpipam_client.client import (
-    PhpIpamClient,
-    PhpIpamException,
-    GET,
-    POST,
-    PATCH,
-    DELETE,
-)
-
-__all__ = [
-    'PhpIpamClient',
-    'PhpIpamException',
-    'GET',
-    'POST',
-    'PATCH',
-    'DELETE',
-]
-'''
