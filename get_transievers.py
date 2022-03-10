10c66f22c4cd46473fc5bf0e369bbd77b015fe51
Daniel WSL2-PC <daniel.federstedt@capio.se>
2022-03-10 15:10:14 +0100

    added workflow for exporting transceivers from switches to csvfile

---
+++ get_transievers.py
@@ -0,0 +1,20 @@
+from pyarubaswitch_workflows.runner import Runner
+from pyarubaswitch_workflows.filehandling import CsvExporter
+
+from pprint import pprint
+
+def main():
+    print("Lets go!")
+    # old switches pre rest-version 7 are very slow over ssl.
+    run = Runner(config_filepath="prod_vars.yaml", verbose=True, SSL=False, rest_version=2, timeout=40)
+
+    switches = run.get_transceivers()
+
+    pprint(switches)
+    exporter = CsvExporter(switches)
+    exporter.export_transceivers_csv("transceivers.csv")
+
+    
+   
+if __name__ == "__main__":
+    main()
