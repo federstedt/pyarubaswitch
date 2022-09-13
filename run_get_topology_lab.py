c4814ce2d34c88b60fe5561737a5671230f628a0
Daniel Federstedt <dfederstedt@gmail.com>
2022-09-13 09:17:20 +0200

    flyttade todo kommentarer

--- run_get_topology_lab.py
+++ run_get_topology_lab.py
@@ -1,6 +1,14 @@
 from pyarubaswitch_workflows.get_topology import TopologyMapper
 
 
+#TODO: fixa när klienter är på trunkar, de är också dubletter men verkar inte träffa bort-sorterings-regelen
+# TODO: hitta fulswitchar / omanagerade switchar som ej är med i ip-listan
+# TODO: förbättra unmaneg listan, ifall netdevices innehåller en mgmt ip som inte är listad i yaml bör den hamna i unmanaged även om den svarar på lldp ?
+# TODO:
+# SNYGGA TILL SCRIPTET EXPORT GÖR FÖR MYCKET I EN FUNKTION
+# Snygga till output och optimera för bättre läsbarhet.
+# Exportera till pdf eller ritning , något roligare än csv ?
+
 def main():
     print("Lets go!")
     # via yaml
