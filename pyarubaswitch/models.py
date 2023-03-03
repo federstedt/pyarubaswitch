85d6c6867050c1997a56af3fd90ce1cf59a7ee49
Daniel Federstedt <daniel@braveops.se>
2023-03-03 16:20:22 +0100

    remade models to BaseModels

--- pyarubaswitch/models.py
+++ pyarubaswitch/models.py
@@ -18,8 +18,6 @@ class STP(BaseModel):
     prio: int
     mode: str
 
-
-
 class LldpNeighbour(BaseModel):
     local_port: int
     name: str
@@ -30,39 +28,20 @@ class LLDPTable(BaseModel):
     switches: list[LldpNeighbour] = []
     access_points: list[LldpNeighbour] = []
 
-#TODO: nedan gör om till BaseModels
 
-class Snmpv3(object):
+class Transceiver(BaseModel):
+    part_number: str
+    port_id: int
+    product_number: str
+    serial_number: str
+    trans_type: str
 
-    def __repr__(self):
-        return f"enabled: {self.enabled}, is_non_v3_readonly: {self.non_snmpv3_readonly}, only_v3: {self.only_v3}"
-
-    def __init__(self, enabled, readonly, only_v3):
-        self.enabled = enabled
-        self.non_snmpv3_readonly = readonly
-        self.only_v3 = only_v3
-
-
-class SntpServer(object):
-
-    def __repr__(self):
-        return f"address: {self.address}, prio: {self.prio}"
-
-    def __init__(self, address, prio):
-        self.address = address
-        self.prio = prio
+class SntpServer(BaseModel):
+    address: str
+    prio: int
 
 
-class Transceiver(object):
-
-    def __repr__(self):
-        return f"Transceiver: part_number: {self.part_number}, port_id: {self.port_id}, product_number: {self.product_number}, serial_number: {self.serial_number}, type: {self.type}" 
-
-    def __init__(self, part_number, port_id, product_number, serial_number, trans_type):
-        self.part_number = part_number
-        self.port_id = port_id
-        self.product_number = product_number
-        self.serial_number = serial_number
-        self.type = trans_type
-
-
+class Snmpv3(BaseModel):
+    enabled: bool
+    readonly: bool
+    only_v3: bool
